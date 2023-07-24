95a01d948a6c9357c1ebcb9dd7cf3dd2e88c5caa
Mohamed Hisham <mohamedhisham67751@gmail.com>
2023-07-24 19:55:53 +0300

    textfield login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -49,36 +51,106 @@ class MyApp extends StatelessWidget {
           child: Icon(Icons.add),
         ),
         body: Padding(
-          padding: const EdgeInsets.only(top: 25, bottom: 50),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Center(
-                  child: Text(
-                    "HELLO",
-                    style: TextStyle(
-                        fontFamily: "my font",
-                        fontSize: 50,
-                        color: Colors.blue),
+          padding: EdgeInsets.all(8),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Center(
+              child: Text(
+                "HELLO",
+                style: TextStyle(
+                    fontFamily: "my font", fontSize: 50, color: Colors.blue),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            // TextField(
+            //   obscureText: true,
+            //   keyboardType: TextInputType.phone,
+            //   textInputAction: TextInputAction.go,
+            //   decoration: InputDecoration(
+            //     labelText: 'txt',
+            //     labelStyle: TextStyle(
+            //         color: Colors.blue, fontFamily: 'my font', fontSize: 20),
+            //     hintText: 'enter text',
+            //     hintStyle: TextStyle(fontFamily: 'my font'),
+            //     border: OutlineInputBorder(
+            //       borderRadius: BorderRadius.circular(50),
+            //     ),
+            //     icon: Icon(Icons.portrait_rounded),
+            //     suffixIcon: Icon(Icons.remove_red_eye_outlined),
+            //   ),
+            // ),
+            Container(
+              width: 250,
+              padding: EdgeInsets.only(left: 30),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.account_circle_outlined),
+                    hintText: 'YOUR EMAIL:'),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              //padding: EdgeInsets.symmetric(horizontal: 30),
+              width: 250,
+              padding: EdgeInsets.only(left: 30),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    //contentPadding: EdgeInsets.only(left: 50),
+                    border: InputBorder.none,
+                    hintText: 'Password :',
+                    icon: Icon(Icons.password),
+                    suffixIcon: Icon(Icons.visibility)),
+              ),
+            ),
+            // Container(
+            //   margin: EdgeInsets.only(bottom: 10),
+            //   decoration: BoxDecoration(
+            //     color: Colors.blue,
+            //     borderRadius: BorderRadius.circular(100),
+            //   ),
+            //   width: 250,
+            //   child: TextButton(
+            //     onPressed: () {
+            //       print('login');
+            //     },
+            //     child: Text(
+            //       'LOGIN',
+            //       style: TextStyle(color: Colors.white, fontSize: 20),
+            //     ),
+            //   ),
+            // ),
+            ElevatedButton(
+              style: ButtonStyle(
+                //backgroundColor: MaterialStateProperty.all(Colors.red),
+                padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(horizontal: 95, vertical: 12)),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
                   ),
                 ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(
-                        'assets/images/facebook.png',
-                        height: 110,
-                      ),
-                      Image.asset(
-                        'assets/images/insta.png',
-                        height: 110,
-                      ),
-                      Image.asset(
-                        'assets/images/twitter.png',
-                        height: 110,
-                      ),
-                    ]),
-              ]),
+              ),
+              onPressed: () {},
+              child: Text(
+                'LOG IN',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ]),
         ),
       ),
     );
